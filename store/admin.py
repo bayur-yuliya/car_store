3119700c337f5fc01ba36d6474d555b09ab51bf4
Yuliya Bayur <juliy14497@gmail.com>
2023-10-21 00:23:00 +0300

    Add admin, change forms, add info in db

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Car, Client, CarType, Licence, Dealership, Order, OrderQuantity
+
+admin.site.register(Client)
+admin.site.register(CarType)
+admin.site.register(Car)
+admin.site.register(Licence)
+admin.site.register(Dealership)
+admin.site.register(Order)
+admin.site.register(OrderQuantity)
