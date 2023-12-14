6b490f11511b11546a4cf827d2f565ccddd83c95
Yuliya Bayur <juliy14497@gmail.com>
2023-12-14 23:46:08 +0200

    make it better

--- store/views.py
+++ store/views.py
@@ -5,10 +5,27 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 
 from .forms import CarPhotoChangeForm
-from .models import Car, Order, OrderQuantity, Dealership, Client, Licence
+from .models import Car, Order, OrderQuantity, Dealership, Client, Licence, CarType
 
 
 def cars(request):
+    CarType.objects.create(name='M135i xDrive', brand='BMW', price=200)
+    CarType.objects.create(name='X7 M60i xDRIVE', brand='BMW', price=100)
+    CarType.objects.create(name='XM LABEL RED', brand='BMW', price=150)
+
+    Car.objects.create(car_type=CarType(id=1), color='red', year=2023)
+    Car.objects.create(car_type=CarType(id=2), color='red', year=2023)
+    Car.objects.create(car_type=CarType(id=3), color='red', year=2023)
+    Car.objects.create(car_type=CarType(id=1), color='black', year=2023)
+    Car.objects.create(car_type=CarType(id=2), color='black', year=2023)
+    Car.objects.create(car_type=CarType(id=3), color='black', year=2023)
+
+    Client.objects.create(name='Tonya', email="test@gmail.com", phone='0387410203')
+    Dealership.objects.create(name='OdessaBMW')
+    my_instance = Dealership.objects.get(pk=1)
+
+    my_instance.available_car_types.set([CarType.objects.get(id=1)])
+    my_instance.clients.set([Client.objects.get(id=1)])
 
     if request.method == "GET":
         all_cars = Car.objects.filter(owner__isnull=True)
