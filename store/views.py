4918e8cb3aff680e6d7c19ff72d7a9f81867e9e9
Yuliya Bayur <juliy14497@gmail.com>
2023-12-14 23:49:45 +0200

    make it better

--- store/views.py
+++ store/views.py
@@ -9,6 +9,9 @@ from .models import Car, Order, OrderQuantity, Dealership, Client, Licence, CarT
 
 
 def cars(request):
+    CarType.objects.create(name='M135i xDrive', brand='BMW', price=200)
+    CarType.objects.create(name='X7 M60i xDRIVE', brand='BMW', price=100)
+    CarType.objects.create(name='XM LABEL RED', brand='BMW', price=150)
 
     Car.objects.create(car_type=CarType(id=1), color='red', year=2023)
     Car.objects.create(car_type=CarType(id=2), color='red', year=2023)
@@ -16,6 +19,13 @@ def cars(request):
     Car.objects.create(car_type=CarType(id=1), color='black', year=2023)
     Car.objects.create(car_type=CarType(id=2), color='black', year=2023)
     Car.objects.create(car_type=CarType(id=3), color='black', year=2023)
+
+    Client.objects.create(name='Tonya', email="test@gmail.com", phone='0387410203')
+    Dealership.objects.create(name='OdessaBMW')
+    my_instance = Dealership.objects.get(pk=1)
+
+    my_instance.available_car_types.set([CarType.objects.get(id=1)])
+    my_instance.clients.set([Client.objects.get(id=1)])
 
     if request.method == "GET":
         all_cars = Car.objects.filter(owner__isnull=True)
