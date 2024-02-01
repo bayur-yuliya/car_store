9293755013d21bfaf1f38c3135cc933f241d9a11
Yuliya Bayur <juliy14497@gmail.com>
2024-02-01 21:04:12 +0200

    Add README.md, github actions, monobank paymant, list purchased cars

--- store/views.py
+++ store/views.py
@@ -1,9 +1,11 @@
 import random
 
-from django.db.models import Count
+from django.core.paginator import Paginator
+from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 
+from api_store.invoices import create_invoice, verify_signature
 from .forms import CarPhotoChangeForm
 from .models import Car, Order, OrderQuantity, Dealership, Client, Licence
 
@@ -11,11 +13,13 @@ from .models import Car, Order, OrderQuantity, Dealership, Client, Licence
 def cars(request):
     if request.method == "GET":
         all_cars = Car.objects.filter(owner__isnull=True)
+
+        paginator = Paginator(all_cars, 6)
+        page_number = request.GET.get("page")
+        page_obj = paginator.get_page(page_number)
+
         blocked_cars = Car.objects.filter(
             blocked_by_order__isnull=False, owner__isnull=True
-        )
-        cars_count = (
-            all_cars.values("car_type", "color").annotate(Count("id")).order_by()
         )
 
         return render(
@@ -23,8 +27,8 @@ def cars(request):
             "store/car_store.html",
             context={
                 "all_cars": all_cars,
-                "cars_count": cars_count,
                 "blocked_cars": blocked_cars,
+                "page_obj": page_obj,
             },
         )
     try:
@@ -100,6 +104,16 @@ def order(request, order_id):
         return redirect(reverse("cars"))
 
     if request.method == "POST":
+        orders = Order.objects.filter(
+            client=Client.objects.get(email=request.user.email)
+        )
+        full_url_webhook = request.build_absolute_uri(reverse("webhook-mono"))
+        full_url_orders = request.build_absolute_uri(
+            reverse("order_is_processed", kwargs={"order_id": order_id})
+        )
+        # invoice_url = create_invoice(orders, full_url_webhook, full_url_orders)
+        invoice_url = create_invoice(orders, full_url_webhook, full_url_orders)
+
         for el in Car.objects.filter(blocked_by_order=order_created.id):
             licence_number = random.randint(1000, 9999)
             if Licence.objects.filter(number=f"BH {licence_number} IT").exists():
@@ -109,7 +123,7 @@ def order(request, order_id):
             order_created.is_paid = True
             order_created.save()
             el.sell()
-        return redirect(reverse("order_is_processed", kwargs={"order_id": order_id}))
+        return redirect(invoice_url)
 
     data = {
         "order_id": order_id,
@@ -121,7 +135,25 @@ def order(request, order_id):
 def order_is_processed(request, order_id):
     if request.GET.get("index_page"):
         return redirect(reverse("cars"))
-
+    if request.GET.get("purchased_cars"):
+        return redirect(reverse("purchased_cars"))
     return render(
         request, "store/order_is_processed.html", context={"order_id": order_id}
+    )
+
+
+def purchased_cars(request):
+    if request.GET.get("index_page"):
+        return redirect(reverse("cars"))
+
+    try:
+        client, existence = Client.objects.get_or_create(
+            name=request.user.username, email=request.user.email
+        )
+    except AttributeError:
+        return redirect("account_login")
+
+    cars_list = Car.objects.filter(owner=client)
+    return render(
+        request, "store/purchased_cars.html", context={"cars_list": cars_list}
     )
