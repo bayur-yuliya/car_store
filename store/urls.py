a7aef0e1147acda6cd6abe7a7385584379ac248f
Yuliya Bayur <juliy14497@gmail.com>
2024-02-01 22:22:58 +0200

    Activate black

--- store/urls.py
+++ store/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path("", views.cars, name="cars"),
     path("order/<int:order_id>", views.order, name="order"),
-    path('update_car/<int:car_id>', views.update_car, name="update_car"),
+    path("update_car/<int:car_id>", views.update_car, name="update_car"),
     path(
         "order_is_processed/<int:order_id>",
         views.order_is_processed,
