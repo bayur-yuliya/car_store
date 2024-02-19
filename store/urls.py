3f4a0cd21abe4476f32371d834d6a94bff0818c7
Yuliya Bayur <juliy14497@gmail.com>
2024-02-19 22:59:53 +0200

    Change button in temaplates, add function add_to_cart

--- store/urls.py
+++ store/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.cars, name="cars"),
     path("order/<int:order_id>", views.order, name="order"),
+    path("add/<int:car_id>", views.add_to_cart, name="add_to_cart"),
     path("update_car/<int:car_id>", views.update_car, name="update_car"),
     path(
         "order_is_processed/<int:order_id>",
