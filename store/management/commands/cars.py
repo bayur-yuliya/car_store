792e10bcc6a63f3f6fe35af123942eb810ac0e52
Baiur Yuliia <juliy14497@gmail.com>
2024-08-01 18:37:12 +0300

    Activate black

--- store/management/commands/cars.py
+++ store/management/commands/cars.py
@@ -11,9 +11,20 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for _ in range(10):
-            Car.objects.create(car_type=CarType(id=random.randint(1, 3)), color="red", year=random.randint(2009, 2025))
-            Car.objects.create(car_type=CarType(id=random.randint(1, 3)), color="white", year=random.randint(2009, 2025))
-            Car.objects.create(car_type=CarType(id=random.randint(1, 3)), color="black", year=random.randint(2009, 2025))
+            Car.objects.create(
+                car_type=CarType(id=random.randint(1, 3)),
+                color="red",
+                year=random.randint(2009, 2025),
+            )
+            Car.objects.create(
+                car_type=CarType(id=random.randint(1, 3)),
+                color="white",
+                year=random.randint(2009, 2025),
+            )
+            Car.objects.create(
+                car_type=CarType(id=random.randint(1, 3)),
+                color="black",
+                year=random.randint(2009, 2025),
+            )
 
         self.stdout.write(self.style.SUCCESS("Все прошло успешно!"))
-        
