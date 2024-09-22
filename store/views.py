68b30ee90e79b816aaa26def76b4c8bf2e111719
Baiur Yuliia <juliy14497@gmail.com>
2024-09-22 18:46:17 +0300

    make it better

--- store/views.py
+++ store/views.py
@@ -123,7 +123,7 @@ def order(request, order_id):
     if request.method == "POST":
         orders = Order.objects.get(client=Client.objects.get(email=request.user.email))
         full_url_webhook = "https://webhook.site/2f8f0a75-24c7-4907-ac1d-efeb1e58d1e8"
-        full_url_orders = f"http://127.0.0.1:8000/order_is_processed/{order_id}"
+        full_url_orders = f"https://sheltered-beach-61324-b8aaf597abf6.herokuapp.com/order_is_processed/{order_id}"
         invoice_url = create_invoice(orders, full_url_webhook, full_url_orders)
 
         for car in Car.objects.filter(blocked_by_order=order_created.id):
